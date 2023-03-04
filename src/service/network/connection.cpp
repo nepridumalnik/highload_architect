d066c5b34b7b80c4143cc11f2cd3d3a908e80c9e
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2023-03-04 20:40:13 +0700

    Добавлено создание потоков при подключении

--- src/service/network/connection.cpp
+++ src/service/network/connection.cpp
@@ -1,5 +1,7 @@
 #include <service/network/connection.hpp>
 
+#include <boost/thread.hpp>
+
 #include <iostream>
 
 Connection::Connection(const size_t port, callback cb)
@@ -15,7 +17,24 @@ void Connection::operator()()
         {
             boost::asio::ip::tcp::socket socket(context_);
             acceptor_.accept(socket);
-            cb_(std::move(socket));
+
+            boost::thread socket_thread(
+                [cb = cb_, socket = std::move(socket)]() mutable
+                {
+                    try
+                    {
+                        cb(std::move(socket));
+                    }
+                    catch (const std::exception &e)
+                    {
+                        std::cerr << "Error: " << e.what() << std::endl;
+                    }
+                    catch (...)
+                    {
+                        std::cerr << "Unknown error" << std::endl;
+                    }
+                });
+            socket_thread.detach();
         }
         catch (const std::exception &e)
         {
