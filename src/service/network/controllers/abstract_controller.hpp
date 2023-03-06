c7922c4fc9d9b6b17d44a472769de805099a8049
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2023-03-06 22:47:35 +0700

    Разделение абстрактного класса

--- src/service/network/controllers/abstract_controller.hpp
+++ src/service/network/controllers/abstract_controller.hpp
@@ -2,11 +2,11 @@
 
 #include <boost/beast.hpp>
 
-/// @brief Интерфейс контроллера
-struct AbstractController
+/// @brief Интерфейс обработчика
+struct AbstractHttpHandler
 {
     /// @brief Деструктор
-    virtual ~AbstractController() = default;
+    virtual ~AbstractHttpHandler() = default;
 
     /// @brief Обработать соединение
     /// @param req Запрос
@@ -14,9 +14,21 @@ struct AbstractController
     virtual void HandleRequest(
         const boost::beast::http::request<boost::beast::http::dynamic_body> &req,
         boost::beast::websocket::response_type &res) = 0;
-    
+};
+
+/// @brief Интерфейс пути
+struct AbstractRoute
+{
+    /// @brief Деструктор
+    virtual ~AbstractRoute() = default;
+
     /// @brief Проверить, обрабатываем ли маршрут
     /// @param route Маршрут
     /// @return True, если обрабатываем
-    virtual bool HasRoute(const std::string& route) = 0;
+    virtual bool HasRoute(const std::string &route) = 0;
+};
+
+/// @brief Интерфейс контроллера
+struct AbstractController : public AbstractHttpHandler, public AbstractRoute
+{
 };
