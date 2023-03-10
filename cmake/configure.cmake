8a09400d91f410b78ac863ec26d794e85a595d3f
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2023-03-10 20:41:42 +0700

    Настроена сборка на всех ОС

--- cmake/configure.cmake
+++ cmake/configure.cmake
@@ -19,9 +19,27 @@ if(WIN32)
     set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /ignore:4099")
     set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /ignore:4099")
     set(CMAKE_STATIC_LINKER_FLAGS "${CMAKE_STATIC_LINKER_FLAGS} /ignore:4099")
+
+    set(SOCI_LIBS
+        ${CONAN_LIBS_SOCI}
+        ${CONAN_LIBS_LIBMYSQLCLIENT}
+        ${CONAN_LIBS_ZLIB}
+        ${CONAN_LIBS_ZSTD}
+        ${CONAN_LIBS_OPENSSL}
+    )
 elseif(UNIX)
     message("-- Setting up for Unix")
 
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC")
+
+    # Рецепт из conancenter поломан, поэтому существует этот костыль
+    # Нужно установить libsoci-dev
+    set(SOCI_MYSQL /usr/lib/x86_64-linux-gnu/libsoci_mysql.so)
+    set(SOCI_CORE /usr/lib/x86_64-linux-gnu/libsoci_core.so)
+    set(SOCI_LIBS ${SOCI_MYSQL} ${SOCI_CORE})
+
+    file(COPY ${SOCI_MYSQL} DESTINATION ${CMAKE_LIBRARY_OUTPUT_DIRECTORY} FOLLOW_SYMLINK_CHAIN)
+    file(COPY ${SOCI_CORE} DESTINATION ${CMAKE_LIBRARY_OUTPUT_DIRECTORY} FOLLOW_SYMLINK_CHAIN)
+    message("-- CMAKE_LIBRARY_OUTPUT_DIRECTORY: ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}")
 endif()
