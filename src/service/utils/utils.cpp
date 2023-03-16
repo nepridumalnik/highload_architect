b76ed52d0a08e729e91c782087b1b0093f470e0f
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2023-03-17 00:37:48 +0700

    Добавлен шаблон поиска хэша

--- src/service/utils/utils.cpp
+++ src/service/utils/utils.cpp
@@ -3,20 +3,33 @@
 #include <boost/uuid/detail/md5.hpp>
 #include <boost/algorithm/hex.hpp>
 
-std::string HashMD5(const std::string &input)
+template <typename T>
+static inline std::string HashMD5(const T &input)
 {
     using boost::uuids::detail::md5;
+    static constexpr size_t md5Len = 32;
 
-    std::string output;
-
-    md5 hash;
-    md5::digest_type digest;
+    md5 hash{};
+    md5::digest_type digest{};
 
     hash.process_bytes(input.data(), input.size());
     hash.get_digest(digest);
 
-    const int *const intDigest = reinterpret_cast<const int *>(&digest);
-    boost::algorithm::hex(intDigest, intDigest + (sizeof(md5::digest_type) / sizeof(int)), std::back_inserter(output));
+    std::string output;
+    output.reserve(md5Len);
+    boost::algorithm::hex(reinterpret_cast<const int *>(&digest),
+                          reinterpret_cast<const int *>(&digest) + (sizeof(md5::digest_type) / sizeof(int)),
+                          std::back_inserter(output));
 
     return output;
+}
+
+std::string HashMD5(const std::vector<uint8_t> &input)
+{
+    return HashMD5<std::vector<uint8_t>>(input);
+}
+
+std::string HashMD5(const std::string &input)
+{
+    return HashMD5<std::string>(input);
 }
--- src/service/utils/utils.hpp
+++ src/service/utils/utils.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+#include <vector>
 #include <string>
+
+/// @brief Просчитать MD5 Хэш
+/// @param input Данные
+/// @return Строка с хэшем
+std::string HashMD5(const std::vector<uint8_t> &input);
 
 /// @brief Просчитать MD5 Хэш
 /// @param input Строка
