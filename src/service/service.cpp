795ce56d9ac254651236a56798662aa4c69ae521
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2023-03-18 20:50:50 +0700

    Добавлено чтение конфигов

--- src/service/service.cpp
+++ src/service/service.cpp
@@ -4,17 +4,61 @@
 #include <service/network/connection.hpp>
 #include <service/network/router.hpp>
 
+#include <service/resources/connection_parameters.hpp>
+#include <service/settings/settings_reader.hpp>
+
 #include <soci/mysql/soci-mysql.h>
 #include <soci/session.h>
 
 #include <boost/thread.hpp>
 #include <boost/asio.hpp>
 
-static const std::string ConnectionInfo = "host=mysql port=3306 dbname=main_database user=root password===PaSsWoRd==";
+#include <vector>
+
+std::string GetConnectionInfo()
+{
+    static const std::vector<std::pair<const std::string, const std::string>> Parameters{
+        {connection_parameters::Host, connection_parameters::DefaultHost},
+        {connection_parameters::Port, connection_parameters::DefaultPort},
+        {connection_parameters::Database, connection_parameters::DefaultDatabase},
+        {connection_parameters::User, connection_parameters::DefaultUser},
+        {connection_parameters::Password, connection_parameters::DefaultPassword},
+    };
+
+    SettingsReader reader;
+
+    if (!reader.ReadFile("settings.json"))
+    {
+        return connection_parameters::DefaultConnectionInfo;
+    }
+
+    std::string connectionInfo;
+
+    for (const auto &parameter : Parameters)
+    {
+        std::string newParameter = reader.GetParameter<std::string>(parameter.first);
+
+        if (newParameter.empty())
+        {
+            newParameter = parameter.second;
+        }
+
+        if (!connectionInfo.empty())
+        {
+            connectionInfo += ' ';
+        }
+
+        connectionInfo += parameter.first + '=' + newParameter;
+    }
+
+    return connectionInfo;
+}
 
 Service::Service()
 {
-    sql_ = std::make_shared<soci::session>(soci::mysql, ConnectionInfo);
+    const std::string connectionInfo = GetConnectionInfo();
+
+    sql_ = std::make_shared<soci::session>(soci::mysql, connectionInfo);
     router_.AddController(std::make_unique<UsersController>(sql_));
 }
 
