f6a3c64b12516a5ef408cd532aa3622abff12dcc
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2023-03-21 21:52:14 +0700

    Правки в нагрузочном тесте

--- tests/k6/search_users.js
+++ tests/k6/search_users.js
@@ -3,19 +3,19 @@ import http from 'k6/http'
 
 export let options = {
     stages: [
-        { duration: '1m', target: 1 },
-        { duration: '1m', target: 1 },
-        { duration: '1m', target: 10 },
-        { duration: '1m', target: 10 },
-        { duration: '1m', target: 100 },
-        { duration: '1m', target: 100 },
-        { duration: '1m', target: 1000 },
-        { duration: '1m', target: 1000 },
-        { duration: '1m', target: 100 },
-        { duration: '1m', target: 100 },
-        { duration: '1m', target: 10 },
-        { duration: '1m', target: 10 },
-        { duration: '1m', target: 1 },
+        { duration: '10s', target: 1 },
+        { duration: '10s', target: 1 },
+        { duration: '10s', target: 10 },
+        { duration: '10s', target: 10 },
+        { duration: '10s', target: 100 },
+        { duration: '10s', target: 100 },
+        { duration: '10s', target: 1000 },
+        { duration: '10s', target: 1000 },
+        { duration: '10s', target: 100 },
+        { duration: '10s', target: 100 },
+        { duration: '10s', target: 10 },
+        { duration: '10s', target: 10 },
+        { duration: '10s', target: 1 },
     ]
 }
 
